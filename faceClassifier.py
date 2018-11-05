d814387d8701039bee7f12732f57aeaea27c0d05
wingerliu <waini455801125@qq.com>
2018-11-05 09:31:17 +0800

    完成人脸识别的功能

--- faceClassifier.py
+++ faceClassifier.py
@@ -1,10 +1,19 @@
 import cv2
 import dlib
+import datetime
+import os
+import numpy as np
+import pandas as pd
 from PyQt5.QtGui import *
 
 
+person_face_csv = './person_face_data.csv'
+facePath = './person_faces/'
+threshold = 0.5
+
 class FaceClassifier():
     def __init__(self, classifier='cnn'):
+
         self.detect_class = classifier
         self.detected_face = None
         self.save_flag = 0
@@ -19,6 +28,11 @@ class FaceClassifier():
         elif classifier == "cnn":
             self.detector = dlib.cnn_face_detection_model_v1('mmod_human_face_detector.dat')
 
+        if not os.path.exists(person_face_csv):
+            self.person_face_data = self.read_face_data() #二维list
+        else:
+            self.person_face_data = pd.read_csv(person_face_csv).values.tolist()
+        print("#########", self.person_face_data)
 
     def reco_cv2(self, img):
         faces = self.detector.detectMultiScale(img, 1.3, 5)  # 1.3和5是特征的最小、最大检测窗口，它改变检测结果也会改变
@@ -52,7 +66,7 @@ class FaceClassifier():
         elif self.detect_class == "dlib":
             dets = self.reco_dlib(show_1)
             if len(dets)==1:
-                self.detected_face = show_0[dets[0].left():dets[0].right(), dets[0].top():dets[0].bottom()]
+                self.detected_face = show_0[dets[0].top():dets[0].bottom(), dets[0].left():dets[0].right()]
                 self.save_flag = 1
             for i, d in enumerate(dets):
                 x1 = d.top() if d.top() > 0 else 0
@@ -60,6 +74,9 @@ class FaceClassifier():
                 x2 = d.left() if d.left() > 0 else 0
                 y2 = d.right() if d.right() > 0 else 0
                 cv2.rectangle(show_1, (x2, x1), (y2, y1), (0, 255, 0), 2)  # 画出人脸
+
+                label = self.get_face_name(show_1[x1:y1, x2:y2])
+                cv2.putText(show_1, str(label), (x2, x1), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2, cv2.LINE_AA)
 
         elif self.detect_class == "cnn":
             dets = self.reco_dlib_cnn(show_1)
@@ -80,21 +97,61 @@ class FaceClassifier():
 
     #保存人脸及人脸标签 输入图像为cv格式
     def face_save(self, face, label):
-        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
 
         # 如果图片太大则压缩图像
         if face.shape[0] * face.shape[1] > 500000:
             face = cv2.resize(face, (0, 0), fx = 0.5, fy = 0.5)
 
+        if not os.path.exists(person_face_csv):
+            self.person_face_data = self.read_face_data()
 
-
+        name = label + datetime.datetime.now().strftime('%H%M%S')
+        # if len(self.person_face_data) != 0:
+        #     if name not in np.array(self.person_face_data)[:, 0].tolist():
+        self.person_face_data.append([name, label, self.get_128d_features(face)])
         return True
+
 
     #返回一张图像多张人脸的 128D 特征, 输入img为分割后的人脸图像
     def get_128d_features(self, img):
-        shape = self.sp(img)
-        face_des.append(self.facerec.compute_face_descriptor(img, shape))
-        else:
-            face_des = []
-        return face_des
+        sp = self.sp(img, dlib.rectangle(0, 0, img.shape[1], img.shape[0]))
+        print(dlib.rectangle(0, 0, img.shape[1], img.shape[0]))
+        face_descriptor = self.facerec.compute_face_descriptor(img, sp)
+        print('--------', np.array(face_descriptor).reshape((1, 128)).tolist()[0])
+
+        return np.array(face_descriptor).reshape((1, 128)).tolist()[0] #以list格式返回
+
+
+    def read_face_data(self):
+        print("从文件夹中读取人脸数据!!!")
+
+        labels = os.listdir(facePath)
+        faces_list = []
+        for label in labels:  #判断人脸文件夹中是否存在人脸
+            faces = os.listdir(os.path.join(facePath, label))
+            if len(faces) > 0:
+                for face in faces:
+                    face_name = os.path.join(facePath, label, face)
+                    if face_name.endswith('.jpg'):
+                        face_img = cv2.imread(face_name) #BGR 图片
+                        face_descip = self.get_128d_features(face_img)
+                        faces_list.append([face, label, face_descip]) #图片名称，姓名，人脸信息
+        pd.DataFrame(faces_list, columns=['face_name', 'label', 'face_descrip']).to_csv(person_face_csv, index=None)
+        return faces_list
+
+
+    def get_face_name(self, face):
+        total_faces_arr = np.array([eval(person[2]) for person in self.person_face_data]) #array 格式
+        one_face_arr = np.array(self.get_128d_features(face))
+
+        temp = one_face_arr - total_faces_arr
+        e = np.linalg.norm(temp, axis=1, keepdims=True)
+        min_distance = e.min()
+        print("distance: ", min_distance)
+        if min_distance > threshold:
+            return 'unknow'
+        index = np.argmin(e)
+
+        return self.person_face_data[index][1]
 
